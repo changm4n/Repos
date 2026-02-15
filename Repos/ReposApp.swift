8b38170ad88878bbb483c7a63b4ef4db2f8839e8
Ayden <lcmini6528@gmail.com>
2026-02-15 19:38:24 +0900

    Initial Commit

---
+++ Repos/ReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReposApp.swift
+//  Repos
+//
+//  Created by Ayden on 2/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
